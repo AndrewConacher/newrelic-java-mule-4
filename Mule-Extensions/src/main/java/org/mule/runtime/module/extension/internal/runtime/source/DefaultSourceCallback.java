2a6c6427862d370fe94e94e727d35b797fe3da8f
Doug Hilpipre <dhilpipre@newrelic.com>
2019-04-03 14:27:31 -0500

    baseline

---
+++ Mule-Extensions/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/DefaultSourceCallback.java
@@ -0,0 +1,17 @@
+package org.mule.runtime.module.extension.internal.runtime.source;
+
+import org.mule.runtime.extension.api.runtime.operation.Result;
+import org.mule.runtime.extension.api.runtime.source.SourceCallbackContext;
+
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@Weave
+abstract class DefaultSourceCallback<T, A> {
+
+	@Trace
+	public void handle(Result<T, A> result, SourceCallbackContext context) {
+		Weaver.callOriginal();
+	}
+}
