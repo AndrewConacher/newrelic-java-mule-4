1b44d85c9051c04e8134b954238bed520ce18c49
Doug Hilpipre <dhilpipre@newrelic.com>
2020-04-29 12:18:16 -0500

    attempt to get rid of non-expiring tokens

--- Mule-Core-4.2.2/src/main/java/org/mule/runtime/core/privileged/processor/chain/AbstractMessageProcessorChain$1.java
+++ Mule-Core-4.2.2/src/main/java/org/mule/runtime/core/privileged/processor/chain/AbstractMessageProcessorChain$1.java
@@ -1,10 +1,8 @@
 package org.mule.runtime.core.privileged.processor.chain;
 
 import org.mule.runtime.core.api.event.CoreEvent;
-import org.mule.runtime.core.internal.event.MuleUtils;
 import org.reactivestreams.Subscription;
 
-import com.newrelic.api.agent.Token;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
@@ -14,12 +12,8 @@ import reactor.util.context.Context;
 @Weave
 class AbstractMessageProcessorChain$1 {
 	
-	@Trace(async=true,excludeFromTransactionTrace=true)
+	@Trace(excludeFromTransactionTrace=true)
 	public void onNext(final CoreEvent event) {
-		Token token = MuleUtils.getToken(event);
-		if(token != null) {
-			token.link();
-		}
 		Weaver.callOriginal();
 	}
 
