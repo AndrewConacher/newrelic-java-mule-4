99420152e47b1bd556b036fc7e1db20fec2433d5
= <dhilpipre@newrelic.com>
2020-11-13 08:16:22 -0600

    fixed dependencies

--- Mule-APIKit-Module_1.3.4/src/main/java/org/mule/module/apikit/Router.java
+++ Mule-APIKit-Module_1.3.4/src/main/java/org/mule/module/apikit/Router.java
@@ -1,30 +1,14 @@
 package org.mule.module.apikit;
 
 import java.lang.reflect.Method;
-import java.util.logging.Level;
 
 import com.newrelic.agent.bridge.AgentBridge;
-import com.newrelic.api.agent.NewRelic;
-import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.WeaveAllConstructors;
-import com.newrelic.api.agent.weaver.Weaver;
 
 @Weave
 public abstract class Router {
 
-//	public abstract String getName();
-	
-//	@Trace(dispatcher=true)
-//	private org.reactivestreams.Publisher<org.mule.runtime.core.api.event.CoreEvent> doRoute(org.mule.runtime.core.api.event.CoreEvent event,org.mule.module.apikit.Configuration config,org.mule.extension.http.api.HttpRequestAttributes attrs)  {
-//		return Weaver.callOriginal();
-//	}
-
-	//	@Trace(dispatcher=true)
-	//	private Publisher<CoreEvent> doRoute(CoreEvent mainEvent, Configuration config, HttpRequestAttributes attributes) {
-	//		return Weaver.callOriginal();
-	//	}
-	
 	@WeaveAllConstructors
 	public Router() {
 		Class<?> thisClass = getClass();
@@ -37,13 +21,4 @@ public abstract class Router {
 		}
 	}
 
-//	@Trace
-//	public CoreEvent process(final CoreEvent event) {
-//		return Weaver.callOriginal();
-//	}
-//	
-//	@Trace
-//	public Publisher<CoreEvent> processEvent(CoreEvent event) {
-//		return Weaver.callOriginal();
-//	}
 }
