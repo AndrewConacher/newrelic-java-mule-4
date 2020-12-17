6da4343f1d28b5285ff839b8296d2f352cd4a2d4
= <dhilpipre@newrelic.com>
2020-12-17 06:42:04 -0600

    fixes to transaction naming and work with new core release

---
+++ Mule-Core-4.3-20200824/main/java/org/mule/runtime/core/api/retry/async/RetryWorker.java
@@ -0,0 +1,39 @@
+package org.mule.runtime.core.api.retry.async;
+
+import java.util.concurrent.Executor;
+
+import org.mule.runtime.api.util.concurrent.Latch;
+import org.mule.runtime.core.api.retry.RetryCallback;
+import org.mule.runtime.core.api.retry.policy.RetryPolicyTemplate;
+
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Token;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@Weave
+public abstract class RetryWorker {
+	
+	private Token token = null;
+	
+	public RetryWorker(RetryPolicyTemplate delegate, RetryCallback callback, Executor workManager, Latch startLatch) {
+		Token t = NewRelic.getAgent().getTransaction().getToken();
+		if(t != null && t.isActive()) {
+			token = t;
+		} else if(t != null) {
+			t.expire();
+			t = null;
+		}
+	}
+	
+	@Trace(async=true)
+	public void run() {
+		if(token != null) {
+			token.linkAndExpire();
+			token = null;
+		}
+		Weaver.callOriginal();
+	}
+
+}
--- Mule-Extensions/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/DefaultSourceCallback.java
+++ Mule-Extensions/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/DefaultSourceCallback.java
@@ -1,17 +1,36 @@
 package org.mule.runtime.module.extension.internal.runtime.source;
 
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.source.SourceCallbackContext;
 
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.api.agent.ApplicationNamePriority;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
+@SuppressWarnings("deprecation")
 @Weave
 abstract class DefaultSourceCallback<T, A> {
 
+	private MuleContext muleContext = Weaver.callOriginal();
+
 	@Trace(dispatcher=true)
 	public void handle(Result<T, A> result, SourceCallbackContext context) {
+		AgentBridge.getAgent().getTransaction(false).setApplicationName(ApplicationNamePriority.REQUEST_ATTRIBUTE, getOwningSourceName());
+		NewRelic.addCustomParameter("Owning-Source-Name", getOwningSourceName() != null ? getOwningSourceName() : "Unnamed");
+		NewRelic.addCustomParameter("Owning-Extension-Name", getOwningExtensionName() != null ? getOwningExtensionName() : "Unnamed");
+		NewRelic.addCustomParameter("MuleContext-ID", muleContext.getId());
+		NewRelic.addCustomParameter("Configuration-Instance-Name", getConfigurationInstance().getName());
 		Weaver.callOriginal();
 	}
+	
+	  public abstract String getOwningSourceName();
+
+	  public abstract String getOwningExtensionName();
+	  
+	  public abstract ConfigurationInstance getConfigurationInstance();
 }
