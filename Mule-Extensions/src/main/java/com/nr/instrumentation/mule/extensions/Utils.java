b16378456bdc12b92e3f5ee23506a8e181ee579f
= <dhilpipre@newrelic.com>
2021-02-25 14:24:57 -0600

    backtrack changes from 4.3

--- Mule-Extensions/src/main/java/com/nr/instrumentation/mule/extensions/Utils.java
+++ Mule-Extensions/src/main/java/com/nr/instrumentation/mule/extensions/Utils.java
@@ -3,6 +3,9 @@ package com.nr.instrumentation.mule.extensions;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mule.runtime.api.component.location.ComponentLocation;
+import org.mule.runtime.core.api.source.MessageSource;
+
 import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.agent.config.AgentConfig;
 import com.newrelic.api.agent.ApplicationNamePriority;
@@ -28,6 +31,26 @@ public class Utils {
 		appNames.add(appName);
 		AgentBridge.getAgent().getTransaction(false).setApplicationName(ApplicationNamePriority.REQUEST_ATTRIBUTE, appName);
 		
+	}
+
+	public static String getLocationName(ComponentLocation location) {
+		if(location == null) return null;
+		
+		String locationName = location.getLocation();
+		if(locationName !=null && !locationName.isEmpty()) {
+			if(locationName.endsWith("/source")) {
+				return locationName.replace("/source", "");
+			}
+			return locationName;
+		}
+		return null;
+	}
+
+	public static String getMessageLocation(MessageSource msgSource) {
+		if(msgSource == null) return  null;
+		
+		ComponentLocation componentLoc = msgSource.getLocation();
+		return getLocationName(componentLoc);
 	}
 
 }
--- Mule-Extensions/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/DefaultSourceCallback.java
+++ Mule-Extensions/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/DefaultSourceCallback.java
@@ -1,12 +1,17 @@
 package org.mule.runtime.module.extension.internal.runtime.source;
 
+import java.util.function.Supplier;
+
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.source.MessageSource;
+import org.mule.runtime.core.privileged.execution.MessageProcessContext;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.source.SourceCallbackContext;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TransactionNamePriority;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.instrumentation.mule.extensions.Utils;
@@ -17,9 +22,25 @@ abstract class DefaultSourceCallback<T, A> {
 
 	private MuleContext muleContext = Weaver.callOriginal();
 
+	private Supplier<MessageProcessContext> processContextSupplier = Weaver.callOriginal();
+
 	@Trace(dispatcher=true)
 	public void handle(Result<T, A> result, SourceCallbackContext context) {
-	
+
+		if(processContextSupplier != null) {
+			MessageProcessContext msgCtx = processContextSupplier.get();
+			if(msgCtx != null) {
+				MessageSource msgSource = msgCtx.getMessageSource();
+				String locName = Utils.getMessageLocation(msgSource);
+				if(locName != null && !locName.isEmpty()) {
+					if(locName.endsWith("/source")) {
+						locName = locName.replace("/source", "");
+					}
+					NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.CUSTOM_LOW, false, "Flows", locName);
+				}
+
+			}
+		}
 		Utils.addAppName(getOwningSourceName());
 		NewRelic.addCustomParameter("Owning-Source-Name", getOwningSourceName() != null ? getOwningSourceName() : "Unnamed");
 		NewRelic.addCustomParameter("Owning-Extension-Name", getOwningExtensionName() != null ? getOwningExtensionName() : "Unnamed");
@@ -27,10 +48,10 @@ abstract class DefaultSourceCallback<T, A> {
 		NewRelic.addCustomParameter("Configuration-Instance-Name", getConfigurationInstance().getName());
 		Weaver.callOriginal();
 	}
-	
-	  public abstract String getOwningSourceName();
 
-	  public abstract String getOwningExtensionName();
-	  
-	  public abstract ConfigurationInstance getConfigurationInstance();
+	public abstract String getOwningSourceName();
+
+	public abstract String getOwningExtensionName();
+
+	public abstract ConfigurationInstance getConfigurationInstance();
 }
